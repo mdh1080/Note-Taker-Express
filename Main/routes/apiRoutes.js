2b66555e6e58a994f9c934d2ae30eae924a9014d
@mdh1080 <110183347+mdh1080@users.noreply.github.com>
2022-11-26 21:24:11 -0500

    Add code store.js and apiRoute

---
+++ Main/routes/apiRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const store = require('../db/store');
+
+//Get api notes responds with all notes from the database
+router.get('/notes', (req, res) => {
+    store
+        .getNotes()
+        .then((notes) => {
+        return res.json(notes);
+        })
+        .catch((err) => res.status(500).json(err));
+});
+
+router.post('/notes', (req, res) => {
+    store
+        .addNote(req.body)
+        .then((note) => res.json(note))
+        .catch((err) => res.status(500).json(err));
+});
+// Delete /api/notes deletes the note with an id equal to req.params.id
+router.delete('/notes/:id', (req, res) => {
+    store
+        .removeNote(req.params.id)
+        .then(() => res.json({ ok: true }))
+        .catch((err) => res.status(500).json(err));
+});
+
+module.exports = router;
