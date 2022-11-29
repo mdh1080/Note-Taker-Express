a6e7a3486bace231df3569bf42f2b6ded940ece9
@mdh1080 <110183347+mdh1080@users.noreply.github.com>
2022-11-29 12:21:15 -0500

    Correct html link to connect files

--- Main/routes/htmlRoutes.js
+++ Main/routes/htmlRoutes.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const router = require('express').Router();
 
+//All other routes respond with the index.html file
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./Public/index.html"));
+});
 //"/notes" responds with the notes.html file
 router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
-//All other routes respond with the index.html file
-router.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "../Public/notes.html"));
 });
 
 module.exports = router;
