20c71cd6009435474f9a2ce2799910f34ef908c2
@mdh1080 <110183347+mdh1080@users.noreply.github.com>
2022-12-01 22:29:07 -0500

    moved index to main so github works

--- Main/routes/htmlRoutes.js
+++ Main/routes/htmlRoutes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 //All other routes respond with the index.html file
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./Public/index.html"));
+    res.sendFile(path.join(__dirname, "./index.html"));
 });
 //"/notes" responds with the notes.html file
 router.get("/notes", (req, res) => {
