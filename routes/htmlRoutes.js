e4047ee296e4963fcd005f759f87c25f2327004c
@mdh1080 <110183347+mdh1080@users.noreply.github.com>
2022-12-01 22:52:04 -0500

    Delete public folder to fix deployment issue

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 
 //All other routes respond with the index.html file
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "./Public/index.html"));
+    res.sendFile(path.join(__dirname, "../index.html"));
 });
 //"/notes" responds with the notes.html file
 router.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "../Public/notes.html"));
+    res.sendFile(path.join(__dirname, "../notes.html"));
 });
 
 module.exports = router;
